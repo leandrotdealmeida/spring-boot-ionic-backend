9d152d2628cf6b595631a4a4b1908e5367e3f3b7
leandro <leandrotrovilho@gmail.com>
2019-10-25 17:21:56 -0300

    Paginacao com parametros opcionais na requisicao

--- src/main/java/com/trovilho/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/trovilho/cursomc/resources/CategoriaResource.java
@@ -5,11 +5,13 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -26,48 +28,61 @@ public class CategoriaResource {
 
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public ResponseEntity<Categoria> find(@PathVariable Integer id) {
-		
+
 		Categoria obj = service.find(id);
 
 		return ResponseEntity.ok().body(obj);
 	}
-	
-	@RequestMapping(method = RequestMethod.GET)
-	public ResponseEntity<List<CategoriaDto> > findAll() {
-		
-		List<Categoria> list = service.findAll();
-		
-		List<CategoriaDto> listDto = list.stream().map(obj -> new CategoriaDto(obj)).collect(Collectors.toList());
-
-		return ResponseEntity.ok().body(listDto);
-	}
 
 	@RequestMapping(method = RequestMethod.POST)
-	public ResponseEntity<Void> insert(@RequestBody Categoria obj1) {	
-		
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj1) {
+
 		Categoria obj = service.insert(obj1);
-		
+
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
-		
+
 		return ResponseEntity.created(uri).build();
 	}
 
 	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
 	public ResponseEntity<Void> update(@RequestBody Categoria obj, @PathVariable Integer id) {
-		
+
 		obj.setId(id);
 		obj = service.update(obj);
 
 		return null;
 	}
-	
+
 	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
-	public ResponseEntity<Void> delete(@PathVariable Integer id){
-		
+	public ResponseEntity<Void> delete(@PathVariable Integer id) {
+
 		service.delete(id);
-		
+
 		return ResponseEntity.noContent().build();
-		
+
+	}
+
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDto>> findAll() {
+
+		List<Categoria> list = service.findAll();
+
+		List<CategoriaDto> listDto = list.stream().map(obj -> new CategoriaDto(obj)).collect(Collectors.toList());
+
+		return ResponseEntity.ok().body(listDto);
+	}
+
+	@RequestMapping(value = "/page", method = RequestMethod.GET)
+	public ResponseEntity<Page<CategoriaDto>> findPage(@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage,
+			@RequestParam(value = "orderBy", defaultValue = "nome") String orderBy,
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction) {
+
+		Page<Categoria> list = service.findPage(page, linesPerPage, orderBy, direction);
+
+		Page<CategoriaDto> listDto = list.map(obj -> new CategoriaDto(obj));
+
+		return ResponseEntity.ok().body(listDto);
 	}
 
 }
