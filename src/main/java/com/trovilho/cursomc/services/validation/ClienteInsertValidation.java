d35095b5d8d1086e047c6ee5eea51ee05670fa99
leandro <leandrotrovilho@gmail.com>
2019-11-05 00:42:27 -0300

    Validacao customizada de CpfOuCnpj

---
+++ src/main/java/com/trovilho/cursomc/services/validation/ClienteInsertValidation.java
@@ -0,0 +1,38 @@
+package com.trovilho.cursomc.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import com.trovilho.cursomc.domain.enums.TipoCliente;
+import com.trovilho.cursomc.dto.ClienteNewDto;
+import com.trovilho.cursomc.resources.exception.FieldMessage;
+import com.trovilho.cursomc.services.validation.utils.BR;
+
+public class ClienteInsertValidation implements ConstraintValidator<ClienteInsert, ClienteNewDto> {
+
+	@Override
+	public void initialize(ClienteInsert ann) {
+	}
+
+	@Override
+	public boolean isValid(ClienteNewDto objDto, ConstraintValidatorContext context) {
+		List<FieldMessage> list = new ArrayList<>();
+		// inclua os testes aqui, inserindo erros na lista
+		if(objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
+			list.add(new  FieldMessage("CpfOuCnpj", "CPF inválido"));
+		}
+		
+		if(objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
+			list.add(new  FieldMessage("CpfOuCnpj", "CNPJ inválido"));
+		}
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+}
