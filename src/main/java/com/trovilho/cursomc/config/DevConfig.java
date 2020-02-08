36f4ef39b3c0dbd8f2b22172be6ce86b0ec076ec
leandro <leandrotrovilho@gmail.com>
2020-02-08 16:52:00 -0300

    Criando profile de sdev

---
+++ src/main/java/com/trovilho/cursomc/config/DevConfig.java
@@ -0,0 +1,33 @@
+package com.trovilho.cursomc.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.trovilho.cursomc.services.DbService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+	
+	@Autowired
+	DbService dbService;
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+	
+	@Bean
+	public boolean instanciateDatabase() throws ParseException {
+		
+		if(!"create".equals(strategy)) {
+			return false;
+		}
+		dbService.instanciateTestDatabase();
+		return true;
+	}
+
+}
