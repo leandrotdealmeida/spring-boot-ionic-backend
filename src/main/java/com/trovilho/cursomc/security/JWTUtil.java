e8ced4bea4a767118803268ccffaf07e2bb88162
leandro <leandrotrovilho@gmail.com>
2020-03-30 11:52:12 -0300

    Classes JwUtil

--- src/main/java/com/trovilho/cursomc/security/JWTUtil.java
+++ src/main/java/com/trovilho/cursomc/security/JWTUtil.java
@@ -4,7 +4,6 @@ import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
