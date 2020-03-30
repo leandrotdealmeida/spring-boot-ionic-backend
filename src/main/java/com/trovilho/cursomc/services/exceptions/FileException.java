9c064d6871f4a38993071d57aa6d91ee99304887
leandro <leandrotrovilho@gmail.com>
2020-03-30 16:43:14 -0300

    Tratando excessoes adequadamente

---
+++ src/main/java/com/trovilho/cursomc/services/exceptions/FileException.java
@@ -0,0 +1,16 @@
+package com.trovilho.cursomc.services.exceptions;
+
+public class FileException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+	public FileException(String message, Throwable cause) {
+		super(message, cause);
+
+	}
+
+	public FileException(String message) {
+		super(message);
+
+	}
+
+}
